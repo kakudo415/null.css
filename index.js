7686edbd8cca42f3ef5d45f1a0966ae1789cbde6
Kakudo <kakudo415@gmail.com>
2018-07-17 12:11:01 +0900

    release js

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+const URL = "https://kakudo415.github.io/null.css/";
+let client = new XMLHttpRequest();
+client.onreadystatechange = () => {
+	if (client.readyState == 4 && client.status == 200) {
+		let version = JSON.parse(client.responseText);
+		let latest = version[version.length - 1];
+		document.write(`<link rel="stylesheet" href="${URL}${latest}/null.css"`);
+	} else {
+		console.error("Failed to load null.css");
+	}
+};
+client.open("GET", URL + "version.json", false);
+client.send();
